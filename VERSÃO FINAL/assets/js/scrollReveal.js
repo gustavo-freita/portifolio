ece1b45dbabec63196978bd7d3a2df650637fa3d
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-07-07 20:02:07 -0300

    :iphone:priemria parte da responsividade das páginas início e sobre feito

--- VERSÃO FINAL/assets/js/scrollReveal.js
+++ VERSÃO FINAL/assets/js/scrollReveal.js
@@ -1,4 +1,4 @@
-ScrollReveal({ 
+/* ScrollReveal({ 
     reset: true,
     distance: '60px',
     duration: 2500,
@@ -6,27 +6,27 @@ ScrollReveal({
 });
 
 /* Animação Global */
-ScrollReveal().reveal('.container__inicio h1, .container__sobre h1, .container__habilidades h1, .container__projetos h1', { delay: 200, origin:'left' });
+/* ScrollReveal().reveal('.container__inicio h1, .container__sobre h1, .container__habilidades h1, .container__projetos h1', { delay: 200, origin:'left' });
 
 ScrollReveal().reveal('.container__inicio .inicio__titulos span, .container__sobre span, .container__habilidades .titulo__habilidades span, .container__projetos span', { delay: 300, origin:'right' });
 
 ScrollReveal().reveal('.primeiraLinha, .segundaLinha, .terceiraLinha', { delay: 400, origin:'top' });
-/* Animação Global */
+ *//* Animação Global */
 
 /* Animação Início */
-ScrollReveal().reveal('.inicio__subTitulo', { delay: 300, origin:'right' });
+/* ScrollReveal().reveal('.inicio__subTitulo', { delay: 300, origin:'right' });
 ScrollReveal().reveal('.botao__curriculo', { delay: 400, origin:'top' });
 ScrollReveal().reveal('.icones__contato a', { delay: 500, origin:'top', interval: 200 });
 ScrollReveal().reveal('.imagem__inicio img', { delay: 500, origin:'bottom' });
-/* Animação Início */
+ *//* Animação Início */
 
 /* Animação Sobre */
-ScrollReveal().reveal('.minha__imagem', { delay: 500, origin:'left' });
+/* ScrollReveal().reveal('.minha__imagem', { delay: 500, origin:'left' });
 ScrollReveal().reveal('.sobre__resumoSobreMim', { delay: 600, origin:'bottom' });
-/* Animação Sobre */
+ *//* Animação Sobre */
 
 /* Animação Habilidades */
-ScrollReveal().reveal('.minhas__habilidades .habilidade', { delay: 300, origin:'top', interval: 200 });
+/* ScrollReveal().reveal('.minhas__habilidades .habilidade', { delay: 300, origin:'top', interval: 200 }); */
 /* Animação Habilidades */
 
 /* Animação Projetos */
