037d0f403d106d61046fedb03413195e49bb0afb
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-07-07 18:41:58 -0300

    :dizzy:arrumando o tempo de delay das animações das páginas

--- VERSÃO FINAL/assets/js/scrollReveal.js
+++ VERSÃO FINAL/assets/js/scrollReveal.js
@@ -6,27 +6,27 @@ ScrollReveal({
 });
 
 /* Animação Global */
-ScrollReveal().reveal('.container__inicio h1, .container__sobre h1, .container__habilidades h1, .container__projetos h1', { delay: 500, origin:'left' });
+ScrollReveal().reveal('.container__inicio h1, .container__sobre h1, .container__habilidades h1, .container__projetos h1', { delay: 200, origin:'left' });
 
-ScrollReveal().reveal('.container__inicio .inicio__titulos span, .container__sobre span, .container__habilidades .titulo__habilidades span, .container__projetos span', { delay: 600, origin:'right' });
+ScrollReveal().reveal('.container__inicio .inicio__titulos span, .container__sobre span, .container__habilidades .titulo__habilidades span, .container__projetos span', { delay: 300, origin:'right' });
+
+ScrollReveal().reveal('.primeiraLinha, .segundaLinha, .terceiraLinha', { delay: 400, origin:'top' });
 /* Animação Global */
 
 /* Animação Início */
-ScrollReveal().reveal('.botao__curriculo', { delay: 700, origin:'top' });
-ScrollReveal().reveal('.inicio__subTitulo', { delay: 600, origin:'right' });
-ScrollReveal().reveal('.imagem__inicio img', { delay: 900, origin:'bottom' });
+ScrollReveal().reveal('.inicio__subTitulo', { delay: 300, origin:'right' });
+ScrollReveal().reveal('.botao__curriculo', { delay: 400, origin:'top' });
 ScrollReveal().reveal('.icones__contato a', { delay: 500, origin:'top', interval: 200 });
+ScrollReveal().reveal('.imagem__inicio img', { delay: 500, origin:'bottom' });
 /* Animação Início */
 
 /* Animação Sobre */
-ScrollReveal().reveal('.minha__imagem', { delay: 700, origin:'left' });
-ScrollReveal().reveal('.primeiraLinha', { delay: 500, origin:'top' });
-ScrollReveal().reveal('.sobre__resumoSobreMim', { delay: 800, origin:'bottom' });
+ScrollReveal().reveal('.minha__imagem', { delay: 500, origin:'left' });
+ScrollReveal().reveal('.sobre__resumoSobreMim', { delay: 600, origin:'bottom' });
 /* Animação Sobre */
 
 /* Animação Habilidades */
-ScrollReveal().reveal('.segundaLinha', { delay: 500, origin:'top' });
-ScrollReveal().reveal('.minhas__habilidades .habilidade', { delay: 500, origin:'top', interval: 200 });
+ScrollReveal().reveal('.minhas__habilidades .habilidade', { delay: 300, origin:'top', interval: 200 });
 /* Animação Habilidades */
 
 /* Animação Projetos */
