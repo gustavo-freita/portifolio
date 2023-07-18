71adec09ea513ec58d4b5887685f68ff83f7bbeb
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-07-18 17:44:30 -0300

    :ok_hand:style:Arrumando estilo e movimentação do slid de projeto

--- VERSÃO FINAL/assets/js/cardSlids.js
+++ VERSÃO FINAL/assets/js/cardSlids.js
@@ -1,21 +1,30 @@
-var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    direction: getDirection(),
+var swiper = new Swiper(".slide__content", {
+    slidesPerView: 1,
+    spaceBetween: 39,
+    loop: true,
+    centerSlid: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
 
-    on: {
-        resize: function () {
-            swiper.changeDirection(getDirection());
-        },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+
+      440: {
+        slidesPerView: 2,
+      },
+
+      800: {
+        slidesPerView: 3,
+      },
     },
-});
-
-function getDirection() {
-    var windowWidth = window.innerWidth;
-    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-
-    return direction;
-}
+  });
--- stile.js
+++ stile.js
@@ -1,7 +1,10 @@
 var swiper = new Swiper(".slide__content", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 39,
     loop: true,
+    centerSlid: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -9,5 +12,19 @@ var swiper = new Swiper(".slide__content", {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+
+      440: {
+        slidesPerView: 2,
+      },
+
+      800: {
+        slidesPerView: 3,
+      },
     },
   });
