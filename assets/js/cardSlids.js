51127d76d7154994ec3fc97b4ceb01842ab8926c
Gustavo Freitas <gustavofreitas_silva@hotmail.com>
2023-07-20 21:16:53 -0300

    :bug:fix:Um bug no codigo JS do swiper dos projetos arrumado

--- assets/js/cardSlids.js
+++ assets/js/cardSlids.js
@@ -1,30 +1,28 @@
 var swiper = new Swiper(".slide__content", {
-    slidesPerView: 1,
-    spaceBetween: 39,
-    loop: true,
-    centerSlid: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints: {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints:{
       0: {
-        slidesPerView: 1,
+          slidesPerView: 1,
       },
-
       520: {
-        slidesPerView: 2,
+          slidesPerView: 2,
       },
-
       800: {
-        slidesPerView: 3,
+          slidesPerView: 3,
       },
-    },
-  });
+  },
+});
